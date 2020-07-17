fde4421b7e4553fa5b4bf53550998add2b5d3da6
hawelkam <m.hawelka@gmail.com>
2020-07-17 20:18:21 +0200

    Method splitting

--- src/main/kotlin/com/michalhawelka/restexample/client/JSONPlaceholderClient.kt
+++ src/main/kotlin/com/michalhawelka/restexample/client/JSONPlaceholderClient.kt
@@ -8,27 +8,33 @@ import org.springframework.http.RequestEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import java.io.File
+import java.lang.Exception
 import java.net.URI
 
 @Service
 class JSONPlaceholderClient {
     private inline fun <reified T: Any> typeRef(): ParameterizedTypeReference<T> = object: ParameterizedTypeReference<T>(){}
 
-
     fun getPosts() {
-        val url = "https://jsonplaceholder.typicode.com/posts"
-        val endpoint = URI.create(url)
-        val request = RequestEntity<Any>(HttpMethod.GET, endpoint)
-        val response = RestTemplate().exchange(request, typeRef<List<PostDTO>>())
-        val items: List<PostDTO> = response.body!!
+        try {
+            val url = "https://jsonplaceholder.typicode.com/posts"
+            val endpoint = URI.create(url)
+            val request = RequestEntity<Any>(HttpMethod.GET, endpoint)
+            val response = RestTemplate().exchange(request, typeRef<List<PostDTO>>())
+            val posts: List<PostDTO> = response.body!!
+            savePostsToFiles(posts)
+        } catch (exception: Exception) {
+            println(exception.message)
+        }
+    }
 
+    private fun savePostsToFiles(posts: List<PostDTO>) {
         val directory = File("posts")
         if (!directory.exists()) {
             directory.mkdir()
         }
-        for(post in items) {
+        for(post in posts) {
             val gsonPretty = GsonBuilder().setPrettyPrinting().create()
-
             val postJson: String = gsonPretty.toJson(post)
             File("posts/" + post.id + ".json").writeText(postJson)
         }
